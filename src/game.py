1544b0800130f9a5cfcf3a35fdf71ca63631760a
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-01-02 16:29:30 -0300

    feat: cria a classe principal do jogo

---
+++ src/game.py
@@ -0,0 +1,48 @@
+import sys
+
+import pygame
+
+from src.config import *
+from src.screens import PlayScreen, SelectScreen
+
+class Game:
+
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.screens = {
+            "play-screen": PlayScreen(self.change_screen),
+            "select-screen": SelectScreen(self.change_screen),
+        }
+
+        self.atual_screen = self.screens["play-screen"]
+
+        self.__setup()
+
+    def __setup(self):
+        self.screen.fill((255, 255, 255))
+        pygame.display.set_caption(GAME_TITLE)
+
+    def __handle_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def change_screen(self, name, human_symbol=None):
+        self.atual_screen = self.screens[name]
+
+        if human_symbol:
+            self.screens["play-screen"].set_human_symbol(human_symbol)
+
+    def run(self) -> None:
+        while True:
+            self.__handle_events()
+
+            dt = pygame.time.get_ticks() / 1000
+            self.atual_screen.show(dt)
+
+            pygame.display.update()
